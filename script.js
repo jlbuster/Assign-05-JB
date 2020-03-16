00745ec389dc15ae0e1c2066e453ae39f25250a0
John Blume <jlblumework@gmail.com>
2020-03-15 21:36:28 -0400

    Added code that will change the input blocks color depending on the time

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let bigBlock = document.getElementById('container')
 let justDay = document.getElementById('currentDay')
 let hours = [9,10,11,12,1,2,3,4,5]
+let now = moment()
+let timeNine = moment('9:00AM', 'h:mmA')
+let timeTen = moment('10:00AM', 'h:mmA')
 
 //Setting the the current day under the title of the page
 justDay.textContent = moment().format('dddd, MMMM Do')
@@ -45,12 +48,126 @@ let inp0 = document.getElementById('input0')
 document.getElementById('button0').addEventListener('click', function(event) {
     event.preventDefault()
 
-    localStorage.setItem('input', inp0.value)
+    inp0.setAttribute('value', localStorage.getItem('input9am'))
 
-    localStorage.getItem('input', inp0.value)
-
-    inp0.value = localStorage.inp0.value
-
-
+    localStorage.setItem('input9am', inp0.value)
 
 })
+
+//Checks the time and adds the correct class based on the time
+function timeCheck0 (past, present, future) {
+    if (past > present) {
+        document.getElementById('input0').classList.add('past')
+    } else if (past < present) {
+        document.getElementById('input0').classList.add('future')
+    } 
+    if (past < future) {
+        document.getElementById('input0').classList.add('present')
+    }
+}
+
+timeCheck0(now, timeNine, timeTen)
+
+function timeCheck1 (past, present, future) {
+    if (past > present) {
+        document.getElementById('input1').classList.add('past')
+    } else if (past < present) {
+        document.getElementById('input1').classList.add('future')
+    } 
+    if (past < future) {
+        document.getElementById('input1').classList.add('present')
+    }
+}
+
+timeCheck1(now, timeNine, timeTen)
+
+function timeCheck2 (past, present, future) {
+    if (past > present) {
+        document.getElementById('input2').classList.add('past')
+    } else if (past < present) {
+        document.getElementById('input2').classList.add('future')
+    } 
+    if (past < future) {
+        document.getElementById('input2').classList.add('present')
+    }
+}
+
+timeCheck2(now, timeNine, timeTen)
+
+function timeCheck3 (past, present, future) {
+    if (past > present) {
+        document.getElementById('input3').classList.add('past')
+    } else if (past < present) {
+        document.getElementById('input3').classList.add('future')
+    } 
+    if (past < future) {
+        document.getElementById('input3').classList.add('present')
+    }
+}
+
+timeCheck3(now, timeNine, timeTen)
+
+function timeCheck4 (past, present, future) {
+    if (past > present) {
+        document.getElementById('input4').classList.add('past')
+    } else if (past < present) {
+        document.getElementById('input4').classList.add('future')
+    } 
+    if (past < future) {
+        document.getElementById('input4').classList.add('present')
+    }
+}
+
+timeCheck4(now, timeNine, timeTen)
+
+function timeCheck5 (past, present, future) {
+    if (past > present) {
+        document.getElementById('input5').classList.add('past')
+    } else if (past < present) {
+        document.getElementById('input5').classList.add('future')
+    } 
+    if (past < future) {
+        document.getElementById('input5').classList.add('present')
+    }
+}
+
+timeCheck5(now, timeNine, timeTen)
+
+function timeCheck6 (past, present, future) {
+    if (past > present) {
+        document.getElementById('input6').classList.add('past')
+    } else if (past < present) {
+        document.getElementById('input6').classList.add('future')
+    } 
+    if (past < future) {
+        document.getElementById('input6').classList.add('present')
+    }
+}
+
+timeCheck6(now, timeNine, timeTen)
+
+function timeCheck7 (past, present, future) {
+    if (past > present) {
+        document.getElementById('input7').classList.add('past')
+    } else if (past < present) {
+        document.getElementById('input7').classList.add('future')
+    } 
+    if (past < future) {
+        document.getElementById('input7').classList.add('present')
+    }
+}
+
+timeCheck7(now, timeNine, timeTen)
+
+function timeCheck8 (past, present, future) {
+    if (past > present) {
+        document.getElementById('input8').classList.add('past')
+    } else if (past < present) {
+        document.getElementById('input8').classList.add('future')
+    } 
+    if (past < future) {
+        document.getElementById('input8').classList.add('present')
+    }
+}
+
+timeCheck8(now, timeNine, timeTen)
